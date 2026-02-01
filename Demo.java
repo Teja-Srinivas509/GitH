a8c80776a9e0afd0276c4a60e50765b308c6d848
= <=>
2026-02-01 16:06:34 +0530

    update newbranch

--- Demo.java
+++ Demo.java
@@ -1,6 +1,6 @@
 public class Demo{
     public static void main(String[] args) {
         System.out.println("Hello world form Demo class from master branch");
-        System.out.println("new code in the new branch");
+        System.out.println("new code in the new branch editing from newbranch");
     }
 }
